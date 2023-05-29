4d79142031a560d0b583426469afc01327d67482
Archvile-KPU <58390793+Archvile-KPU@users.noreply.github.com>
2023-05-30 06:45:44 +0900

    Update radiation.py
    added function - graph of radiation level of all facilities(test value)

--- radiation.py
+++ radiation.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 30 06:24:54 2023
+
+@author: MK
+"""
+
 import requests
 import tkinter as tk
 from tkinter import ttk
@@ -59,5 +66,32 @@ fetch_button.pack()
 # Content for the Blank tab (tab_blank)
 blank_label = tk.Label(tab_blank, text="This is a blank page.")
 blank_label.pack()
+
+
+height=400
+width=600
+
+fclist = ['월성','고리','한빛','한울','새울']
+radlist = [40,56,99,0,30]
+
+canvas = tk.Canvas(blank_label,width=width,height=height)
+canvas.pack()
+
+histogram = [0 for _ in range(5)]
+#for _ in range(1000):
+    #histogram[random.randint(0,4)] += 1
+for i in range(5):
+    histogram[i] += radlist[i]
+canvas.create_line(10,height-10,width-10,height-10,tags='histogram')
+barwidth = (width-20)/5
+maxcount = max(histogram)
+index=0
+for i in range(5):
+    canvas.create_text(10+i*barwidth+(barwidth/2),height-5,text=fclist[index],tags='histogram')
+    index+=1
+    canvas.create_rectangle(10+i*barwidth,height-(height-10)*histogram[i]/maxcount,10+(i+1)*barwidth,height-10,tags='histogram')
+    canvas.create_text(10+i*barwidth+(barwidth/2),height-(height)*histogram[i]/maxcount,text=str(histogram[i]),tags='histogram')
+
+
 
 window.mainloop()
