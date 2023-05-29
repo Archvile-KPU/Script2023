928f660f1cb72c39b4c43950a1e86ba77486d770
Archvile-KPU <58390793+Archvile-KPU@users.noreply.github.com>
2023-05-30 05:33:03 +0900

    Update radiation.py
    added radio buttons for each facilities

--- radiation.py
+++ radiation.py
@@ -2,8 +2,9 @@ import requests
 import tkinter as tk
 
 def get_radiation_level():
+    selected_plant = power_plant.get()  # Get the selected power plant
+    params = {'serviceKey': 'put_key_here', 'genName': selected_plant}
     url = 'http://data.khnp.co.kr/environ/service/realtime/radiorate'
-    params = {'serviceKey': 'put_key_here', 'genName': 'put_gen_name_here'}
     response = requests.get(url, params=params)
     radiation_data = response.content
     radiation_label.config(text="Radiation Level: {}".format(radiation_data))
@@ -13,6 +14,25 @@ window.title("Radiation Level at Nuclear Plants")
 
 radiation_label = tk.Label(window, text="Radiation Level:")
 radiation_label.pack()
+
+# Create radio buttons for power plants
+power_plant = tk.StringVar()
+power_plant.set("WS")  # Set default selection to WS
+
+radio_button_ws = tk.Radiobutton(window, text="WS", variable=power_plant, value="WS")
+radio_button_ws.pack()
+
+radio_button_kr = tk.Radiobutton(window, text="KR", variable=power_plant, value="KR")
+radio_button_kr.pack()
+
+radio_button_yk = tk.Radiobutton(window, text="YK", variable=power_plant, value="YK")
+radio_button_yk.pack()
+
+radio_button_uj = tk.Radiobutton(window, text="UJ", variable=power_plant, value="UJ")
+radio_button_uj.pack()
+
+radio_button_su = tk.Radiobutton(window, text="SU", variable=power_plant, value="SU")
+radio_button_su.pack()
 
 fetch_button = tk.Button(window, text="Fetch Radiation Level", command=get_radiation_level)
 fetch_button.pack()
